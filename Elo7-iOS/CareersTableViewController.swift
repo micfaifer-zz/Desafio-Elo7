21b8fafa934a6e25ea2fb81ebf4c89d2176fb797
Michelle Faifer <micfaifer@gmail.com>
2018-09-19 12:30:57 -0300

    Arrumei o picker de departamentos e o layout da primeira celula da table view

--- Elo7-iOS/CareersTableViewController.swift
+++ Elo7-iOS/CareersTableViewController.swift
@@ -9,16 +9,44 @@
 import UIKit
 
 class CareersTableViewController: UITableViewController {
+    let departments = [String](arrayLiteral: "Engenharia", "Comunicação")
 
-    @IBOutlet weak var areasTextField: UITextField!
+    @IBOutlet weak var departmentTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupDepartmentTextField()
+    }
+
+    func setupDepartmentTextField() {
+        let dataPicker = UIPickerView()
+        dataPicker.delegate = self
+        dataPicker.dataSource = self
+        departmentTextField.inputView = dataPicker
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+}
+
+// MARK: - picker view data source and delegate
+extension CareersTableViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return departments.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return departments[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.departmentTextField.text = self.departments[row]
     }
 }
